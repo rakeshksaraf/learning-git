2c4189c49fd102ca00904df0f7ac091426bfc6b3
rakeshksaraf@gmail.com <rakeshksaraf@gmail.com>
2021-07-29 22:10:30 -0700

    added Github

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello Git World, using vi");
 console.log("Trying with nano, previous was with vim");
 console.log("Again with vi, need to enter i to insert which is not needed in nano");
+console.log("Git and Github")
