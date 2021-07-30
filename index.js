72e594334d9b3e628bf2e15c5a047f9df4868d7d
rakeshksaraf@gmail.com <rakeshksaraf@gmail.com>
2021-07-29 22:33:50 -0700

    test added

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("Hello Git World, using vi");
 console.log("Trying with nano, previous was with vim");
 console.log("Again with vi, need to enter i to insert which is not needed in nano");
 console.log("Git and Github")
+console.log("More to test")
